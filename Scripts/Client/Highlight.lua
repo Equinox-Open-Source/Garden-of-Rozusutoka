6bc28c1757934cf6b36fdcd3a2553ad325476e4a
InsulatorGMan <grantbrown235@gmail.com>
2023-01-15 13:36:21 -0600

    Create Highlight.lua

---
+++ Scripts/Client/Highlight.lua
@@ -0,0 +1,11 @@
+local Player = game.Players.LocalPlayer
+
+local PlayerMouse = Player:GetMouse()
+
+local PickupObjects = workspace.PickupObjects
+
+game.ReplicatedStorage.Events.HighlightClient.OnClientEvent:Connect(function(obj, bool)
+	
+	obj:WaitForChild('Highlight').Enabled = bool
+	
+end)

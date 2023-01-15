7536fac18b845dfa7575d08c569c84f24151372b
InsulatorGMan <grantbrown235@gmail.com>
2023-01-15 13:27:57 -0600

    Create Items.lua

---
+++ Modules/Items.lua
@@ -0,0 +1,36 @@
+local Items = {
+	
+	["PickupObjects"] = {
+		
+		["Chair"] = {
+			
+			["Rarity"] = "Limited",
+			["MaxHeld"] = 70,
+			["DisplayName"] = "Japanese Chair",
+			["Description"] = "Trrrrrrr Shtibididob dob dob dob dob Yes yes yes yes Shtib shtibidid",
+			["Type"] = "Collectible",
+			["ImageId"] = nil,
+			["UseAnimation"] = nil
+			
+		}
+		
+	},
+	["Config"] = {
+		
+		["Common"] = Color3.fromRGB(255,255,255),
+		["Uncommon"] = Color3.fromRGB(110, 153, 202),
+		["Rare"] = Color3.fromRGB(75, 151, 75),
+		["Unique"] = Color3.fromRGB(255, 152, 220),
+		["Epic"] = Color3.fromRGB(33, 84, 185),
+		["Legendary"] = Color3.fromRGB(239, 184, 56),
+		["Mythical"] = Color3.fromRGB(170, 0, 170),
+		["Ethereal"] = Color3.fromRGB(9, 137, 207),
+		["Transcendent"] = Color3.fromRGB(255,0,0),
+		["Limited"] = Color3.fromRGB(255,100,0)
+		
+	}
+	
+}
+
+
+return Items

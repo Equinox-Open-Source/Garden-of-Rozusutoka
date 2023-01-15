dda8554e592e619d5d5421002400250dc098d0e3
InsulatorGMan <grantbrown235@gmail.com>
2023-01-15 13:26:38 -0600

    Create GuiService.lua

---
+++ Modules/GuiService.lua
@@ -0,0 +1,50 @@
+local UIS = game:GetService('UserInputService')
+
+local GuiService = {}
+
+function GuiService:RGB(Item, Speed)
+	
+	if Item:IsA('Frame') then
+		
+		while true do
+
+			for i = 0,1,0.001*Speed do
+
+				Item.BackgroundColor = Color3.fromHSV(i,1,1)
+				wait()
+
+			end
+
+		end
+		
+	elseif Item:IsA('ImageLabel') then
+	
+		while true do
+
+			for i = 0,1,0.001*Speed do
+
+				Item.ImageColor3 = Color3.fromHSV(i,1,1)
+				wait()
+
+			end
+
+		end	
+		
+	elseif Item:IsA('TextLabel') or Item:IsA('TextButton') then
+		
+		while true do
+
+			for i = 0,1,0.001*Speed do
+
+				Item.Color = Color3.fromHSV(i,1,1)
+				wait()
+
+			end
+
+		end
+		
+	end
+	
+end
+
+return GuiService

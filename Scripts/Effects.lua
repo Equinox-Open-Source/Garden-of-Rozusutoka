23cde4ae5ae3b9bf7fc7e605acdd6d2925b41e32
InsulatorGMan <grantbrown235@gmail.com>
2023-01-15 13:28:56 -0600

    Create Effects.lua

---
+++ Scripts/Effects.lua
@@ -0,0 +1,26 @@
+local Effects = {
+	
+	["Poison"] = {
+		
+		["Decal"] = ""
+		
+	}
+	
+}
+
+local EffectPlayerEvent = game.ReplicatedStorage.Events.EffectPlayer
+
+EffectPlayerEvent.OnServerEvent:Connect(function(plr, duration, effect, dmg)
+	
+	if Effects[effect] and effect == "Poison" then
+		
+		for i = 1,duration do
+			
+			plr.Character.Humanoid.Health -= dmg
+			wait(1)
+			
+		end
+		
+	end
+	
+end)
